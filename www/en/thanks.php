6970b675c7da677086e557fbcd6e0f8307766ff4
Wyatt <wyatt@digitalcave.ca>
2006-06-10 18:37:08 +0000

    Updated changelog and webpage

--- www/en/thanks.php
+++ www/en/thanks.php
@@ -8,9 +8,11 @@ Special thanks go out to the people who helped in the creation of Buddi.  These 
 
 <ul>
 <li>My wife, Monica, for putting up with me during the long coding sessions</li>
+<li>Torsten Hinsche for the German translation of Buddi</li>
 <li><a href='http://www.randelshofer.ch/quaqua/download.html'>Quaqua</a>: Native Macintosh look and feel for Java applications</li>
 <li><a href='http://www.jfree.org/jfreechart/'>JFreeChart</a>: Charting library for Java</li>
 <li><a href='http://homepage.mac.com/sroy/mrjadapter/'>MRJAdapter</a>: Library which helps in the creation of native-feeling cross platform Java applications</li>
+<li><a href='http://www.toedter.com/en/jcalendar/'>JCalendar</a>: Swing date chooser widgets</li>
 <li><a href='http://www.eclipse.org/'>Eclipse</a> and <a href='http://www.eclipse.org/emf/'>Eclipse EMF</a></li>
 <li><a href='http://sourceforge.net/'>Sourceforge.net</a>: Hosting for many open source Unix applications</li>
 </ul>
