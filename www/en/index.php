713926c9be77f4e591404d6c17d71b6d27a2e167
Wyatt <wyatt@digitalcave.ca>
2006-06-20 20:24:44 +0000

    Updated homepage languages

--- www/en/index.php
+++ www/en/index.php
@@ -10,13 +10,15 @@ Buddi is a personal finance and budgeting program, aimed at those who have littl
 Buddi will run on almost any computer which has a Java virtual machine installed.  This can include Windows, Macintosh OS X, Linux, and many other operating systems.  One thing to note is that you must have Java 1.5 or later installed.  If you are running on Macintosh OS X 10.4 (Tiger), you should have this installed.  If you are running on a different operating system, like Windows, please consult the help files.  If you need to download a copy of the Java VM, you can do so from <a href='http://java.sun.com/j2se/1.5.0/download.jsp'>here</a>.
 </p>
 <p>
-Buddi is currently available in English, and there are plans for Spanish, French, German and Russian translations in the near future.  If you are interested in translating to another language, feel free to do so!  I would greatly appreciate it if you also sent me the translated file once you are finished.  For more details, please see the <a href='./developers.php'>developers</a> page.
+Buddi is currently available in English and German, and there are plans for Spanish, French and Russian translations in the near future.  If you are interested in translating to another language, feel free to do so!  I would greatly appreciate it if you also sent me the translated file once you are finished.  For more details, please see the <a href='./developers.php'>developers</a> page.
 </p>
 <p>
 Buddi is released under the GNU General Public License (or in other words, this program is Open Source).  This basically means that you can use it for free, as long as you don't try to charge other people for it.  Since you have access to the source code (it is included with every package that you can download), you have the ability to modify the program if you want it to work a little differently.  For more information on <a href='http://en.wikipedia.org/wiki/Open_source'>Open Source Software</a> in general, and the <a href='http://en.wikipedia.org/wiki/GPL'>GNU General Public License</a> in particular, you can refer to Wikipedia.
 </p>
 <p>
-If you decide to use this software, I would love to hear from you!  For suggestions, bug reports, or just to say hi, you can email me at <a href='&#109;&#097;&#105;&#108;&#116;&#111;:&#119;&#121;&#097;&#116;&#116;&#046;&#111;&#108;&#115;&#111;&#110;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;'>&#119;&#121;&#097;&#116;&#116;&#046;&#111;&#108;&#115;&#111;&#110;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a>
+If you decide to use this software, I would love to hear from you!  For suggestions, bug reports, or just to say hi, you can email me at <a 
+href='&#109;&#097;&#105;&#108;&#116;&#111;:&#119;&#121;&#097;&#116;&#116;&#046;&#111;&#108;&#115;&#111;&#110;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;'>&#119;&#121;&#097;&#116;&#116;&#046;&#111;&#108;&#115;&#111;&#110;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;
+</a>
 </p>
 
 <? include "./inc/footer.php" ?>
