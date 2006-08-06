cbd9263dea6a8b6a42455e9effee05b1bed0dcde
Wyatt <wyatt@digitalcave.ca>
2006-08-06 21:02:28 +0000

    1.3.0 Development branch

--- src/org/homeunix/drummer/Const.java
+++ src/org/homeunix/drummer/Const.java
@@ -5,8 +5,8 @@ package org.homeunix.drummer;
 
 
 public class Const {
-	public static final String VERSION = "1.2.0";
-	public static final String BRANCH = "STABLE";
+	public static final String VERSION = "1.3.0";
+	public static final String BRANCH = "UNSTABLE";
 	
 	public final static String LANGUAGE_EXTENSION = ".lang";
 	public final static String LANGUAGE_FOLDER = "Languages";
