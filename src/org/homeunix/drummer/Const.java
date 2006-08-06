26fcc853898f8dfa064e28259962756fbedd14da
Wyatt <wyatt@digitalcave.ca>
2006-08-06 20:45:39 +0000

    Version 1.2.0

--- src/org/homeunix/drummer/Const.java
+++ src/org/homeunix/drummer/Const.java
@@ -5,8 +5,8 @@ package org.homeunix.drummer;
 
 
 public class Const {
-	public static final String VERSION = "1.2.0rc2";
-	public static final String BRANCH = "UNSTABLE";
+	public static final String VERSION = "1.2.0";
+	public static final String BRANCH = "STABLE";
 	
 	public final static String LANGUAGE_EXTENSION = ".lang";
 	public final static String LANGUAGE_FOLDER = "Languages";
