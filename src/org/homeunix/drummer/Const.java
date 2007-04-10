4ccee0a680024b41e800e34c4b399838289db0a9
Wyatt <wyatt@digitalcave.ca>
2007-04-10 15:05:21 +0000

    2.3.3

--- src/org/homeunix/drummer/Const.java
+++ src/org/homeunix/drummer/Const.java
@@ -19,7 +19,7 @@ public class Const {
 	public static final String UNSTABLE = "UNSTABLE";
 	
 	//Version variables
-	public static final String VERSION = "2.3.2";
+	public static final String VERSION = "2.3.3";
 	public static final String BRANCH = UNSTABLE;
 	public static final boolean DEVEL = true;
 	
