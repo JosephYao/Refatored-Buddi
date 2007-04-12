25e5ae8e7c9daae7b43bb1f767d2fb02cc6a3c75
Wyatt <wyatt@digitalcave.ca>
2007-04-12 05:47:47 +0000

    Updated Moss to support new formatter

--- src/org/homeunix/drummer/Const.java
+++ src/org/homeunix/drummer/Const.java
@@ -19,7 +19,7 @@ public class Const {
 	public static final String UNSTABLE = "UNSTABLE";
 	
 	//Version variables
-	public static final String VERSION = "2.3.3";
+	public static final String VERSION = "2.3.4";
 	public static final String BRANCH = UNSTABLE;
 	public static final boolean DEVEL = true;
 	
