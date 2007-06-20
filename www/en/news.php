3af6c2eb46de28d2329389d884124dfc5963775c
Wyatt <wyatt@digitalcave.ca>
2007-06-20 01:46:39 +0000

    Updated web site

--- www/en/news.php
+++ www/en/news.php
@@ -2,9 +2,8 @@
 
 <h1>News</h1>
 
-<h2>This page is out of date!</h2>
 <p>
-As you have no doubt noticed, this page is rather outdated.  This is not due (entirely) to my laziness; instead, I have been too busy updating other documents to remember to update this as well.  Your best bet to find current news is to view the <a href='http://sourceforge.net/news/?group_id=167026'>Buddi News listing</a> on the Sourceforge.net page.  Not only is it more up-to-date, it also has <a href='http://sourceforge.net/export/rss2_project.php?group_id=167026'>RSS feeds</a>!
+Please refer to the <a href='http://sourceforge.net/news/?group_id=167026'>Buddi News listing</a> on the Sourceforge.net page for current news.  Not only is it more up-to-date, it also has <a href='http://sourceforge.net/export/rss2_project.php?group_id=167026'>RSS feeds</a> for your convenience!
 </p>
 
 <? include "./inc/footer.php" ?>
