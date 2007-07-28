6b8d0ee30cc5da906b0d43abc1a1ebb37680ec5c
Wyatt <wyatt@digitalcave.ca>
2007-07-28 15:27:50 +0000

    Updated Const.API_VERSION to 2.6.

--- src/org/homeunix/drummer/Const.java
+++ src/org/homeunix/drummer/Const.java
@@ -24,7 +24,7 @@ public class Const {
 	public static final String VERSION = "2.5.7.0";
 	public static final String BRANCH = UNSTABLE;
 	public static final boolean DEVEL = true;
-	public static final Version API_VERSION = new Version("2.4");
+	public static final Version API_VERSION = new Version("2.6");
 	
 	//Language constants
 	public final static String LANGUAGE_EXTENSION = ".lang";
