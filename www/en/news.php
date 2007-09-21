73fed28a993008cd005680c015082a667fa02a8c
Wyatt <wyatt@digitalcave.ca>
2007-09-21 00:31:22 +0000

    Removed web pages - these are now in a separate repository

--- www/en/news.php
+++
@@ -1,9 +0,0 @@
-<? include "./inc/header.php" ?>
-
-<h1>News</h1>
-
-<p>
-Please refer to the <a href='http://sourceforge.net/news/?group_id=167026'>Buddi News listing</a> on the Sourceforge.net page for current news.  Not only is it more up-to-date, it also has <a href='http://sourceforge.net/export/rss2_project.php?group_id=167026'>RSS feeds</a> for your convenience!
-</p>
-
-<? include "./inc/footer.php" ?>
