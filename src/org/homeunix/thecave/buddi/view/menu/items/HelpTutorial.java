881ebe5a6a6a7e37ba5eeb597a83b29b0370234d
Wyatt <wyatt@digitalcave.ca>
2007-12-01 22:17:58 +0000

    Updated Tutrial page in help menu

--- src/org/homeunix/thecave/buddi/view/menu/items/HelpTutorial.java
+++ src/org/homeunix/thecave/buddi/view/menu/items/HelpTutorial.java
@@ -28,7 +28,7 @@ public class HelpTutorial extends MossMenuItem {
 			bl.openURLinBrowser(
 					Const.PROJECT_URL 
 					+ PrefsModel.getInstance().getLanguage().replaceAll("-.*$", "") 
-					+ "tutorial.php");
+					+ "quickstart_intro.php");
 		}
 		catch (Exception ex){
 			Log.error(ex);
