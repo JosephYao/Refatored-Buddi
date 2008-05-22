18d316ad3258ad8dfc434a8c18725ad8152972af
Wyatt <wyatt@digitalcave.ca>
2008-05-22 03:07:33 +0000

    Added the ability for users to override the default .css files

--- src/org/homeunix/thecave/buddi/Const.java
+++ src/org/homeunix/thecave/buddi/Const.java
@@ -14,7 +14,6 @@ import org.homeunix.thecave.buddi.model.prefs.PrefsModel;
 import org.homeunix.thecave.buddi.plugin.builtin.cellrenderer.ChequeTransactionCellRenderer;
 import org.homeunix.thecave.buddi.plugin.builtin.cellrenderer.DefaultTransactionCellRenderer;
 import org.homeunix.thecave.buddi.plugin.builtin.cellrenderer.SimpleTransactionCellRenderer;
-import org.homeunix.thecave.buddi.plugin.builtin.imports.ImportTestData;
 import org.homeunix.thecave.buddi.plugin.builtin.preference.AdvancedPreferences;
 import org.homeunix.thecave.buddi.plugin.builtin.preference.LocalePreferences;
 import org.homeunix.thecave.buddi.plugin.builtin.preference.NetworkPreferences;
