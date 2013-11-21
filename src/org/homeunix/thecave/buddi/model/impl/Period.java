b4c973d6623ab65dabf832d3fc4c102adb01bec5
josephyao <dermott@126.com>
2013-11-21 16:38:18 +0800

    Introduce domain object Period

---
+++ src/org/homeunix/thecave/buddi/model/impl/Period.java
@@ -0,0 +1,23 @@
+package org.homeunix.thecave.buddi.model.impl;
+
+import java.util.Date;
+
+public class Period {
+	
+	private final Date startDate;
+	private final Date endDate;
+
+	public Period(Date startDate, Date endDate) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+}
