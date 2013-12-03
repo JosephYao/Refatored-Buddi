100e5cd094055da963027a7a27be10085e86f70d
josephyao <dermott@126.com>
2013-12-04 02:17:11 +0800

    Live refactoring code demo in ICBC training

---
+++ src/org/homeunix/thecave/buddi/model/impl/Period.java
@@ -0,0 +1,42 @@
+package org.homeunix.thecave.buddi.model.impl;
+
+import java.util.Date;
+
+import ca.digitalcave.moss.common.DateUtil;
+
+public class Period {
+	
+	private final Date startDate;
+	private final Date endDate;
+
+	public Period(Date startDate, Date endDate) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public boolean equals(Object object) {
+		Period another = (Period) object;
+		return another.startDate.equals(startDate) &&
+			   another.endDate.equals(endDate);
+	}
+
+	public long getDayCount() {
+		return DateUtil.getDaysBetween(startDate, endDate, true);
+	}
+
+	public long getOverlappingDayCount(Period period) {
+		Date overlappingStartDate = (startDate.after(period.startDate)) ? startDate : period.startDate;
+		Date overlappingEndDate = (endDate.before(period.endDate)) ? endDate : period.endDate;
+		
+		return new Period(overlappingStartDate, overlappingEndDate).getDayCount();
+	}
+
+}
